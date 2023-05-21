1161c842fc4837767f9577ce30a630a3a4aee63c
immimis <ngan.htt215435@sis.hust.edu.vn>
2023-05-21 22:45:01 +0700

    Sok Sokong

--- Learning System/Learning System/AddingNewQuiz.Designer.cs
+++ Learning System/Learning System/AddingNewQuiz.Designer.cs
@@ -549,12 +549,130 @@ partial class AddingNewQuiz
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 53;
             this.pictureBox1.TabStop = false;
+            //
+            // Picturebox2 
+            //
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2.Image = System.Drawing.Image.FromFile(@"D:\icon4.png");
+            this.pictureBox2.Location = new System.Drawing.Point(3,35);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox1";
+            this.pictureBox2.Size = new System.Drawing.Size(20,35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 54;
+            this.pictureBox2.TabStop = false;
+            //
+            // Picturebox3 
+            //
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3.Image = System.Drawing.Image.FromFile(@"D:\icon1.png");
+            this.pictureBox3.Location = new System.Drawing.Point(374,42);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox1";
+            this.pictureBox3.Size = new System.Drawing.Size(16,16);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 55;
+            this.pictureBox3.TabStop = false;
+            //
+            // Picturebox4 
+            //
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4.Image = System.Drawing.Image.FromFile(@"D:\icon2.png");
+            this.pictureBox4.Location = new System.Drawing.Point(235,135);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox1";
+            this.pictureBox4.Size = new System.Drawing.Size(16,16);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 56;
+            this.pictureBox4.TabStop = false;
+            //
+            // Picturebox5 
+            //
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5.Image = System.Drawing.Image.FromFile(@"D:\icon1.png");
+            this.pictureBox5.Location = new System.Drawing.Point(245,525);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox1";
+            this.pictureBox5.Size = new System.Drawing.Size(16,16);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 57;
+            this.pictureBox5.TabStop = false;
+            //
+            // Picturebox6 
+            //
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6.Image = System.Drawing.Image.FromFile(@"D:\icon1.png");
+            this.pictureBox6.Location = new System.Drawing.Point(245,633);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox1";
+            this.pictureBox6.Size = new System.Drawing.Size(16,16);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 58;
+            this.pictureBox6.TabStop = false;
+            //
+            // Picturebox7 
+            //
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7.Image = System.Drawing.Image.FromFile(@"D:\icon1.png");
+            this.pictureBox7.Location = new System.Drawing.Point(245,685);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox7.Name = "pictureBox1";
+            this.pictureBox7.Size = new System.Drawing.Size(16,16);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 59;
+            this.pictureBox7.TabStop = false;
+            //
+            // Picturebox8
+            //
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8.Image = System.Drawing.Image.FromFile(@"D:\icon1.png");
+            this.pictureBox8.Location = new System.Drawing.Point(577,440);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(16,16);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 60;
+            this.pictureBox8.TabStop = false;
+            //
+            // Picturebox9
+            //
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9.Image = System.Drawing.Image.FromFile(@"D:\icon3.png");
+            this.pictureBox9.Location = new System.Drawing.Point(760,530);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(25,20);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 61;
+            this.pictureBox9.TabStop = false;
+            //
+            // Picturebox10
+            //
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10.Image = System.Drawing.Image.FromFile(@"D:\icon3.png");
+            this.pictureBox10.Location = new System.Drawing.Point(760,580);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(25,20);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox10.TabIndex = 62;
+            this.pictureBox10.TabStop = false;
+        
             // 
             // AddingNewQuiz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.ImportForm_ExpandPic1);
             this.Controls.Add(this.AddingNewQuizForm_errorTextLbl);
             this.Controls.Add(this.AddingNewQuizForm_timeLimitTxt);
@@ -602,11 +720,18 @@ partial class AddingNewQuiz
             ((System.ComponentModel.ISupportInitialize)(this.AddingNewQuizForm_closeMinuteNud)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ImportForm_ExpandPic1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
     }
-
     #endregion
 
     private Label AddNewQuizForm_HeadingLbl;
@@ -644,6 +769,15 @@ partial class AddingNewQuiz
     private Label AddingNewQuizForm_errorTextLbl;
     private PictureBox ImportForm_ExpandPic1;
     private PictureBox pictureBox1;
+    private PictureBox pictureBox2;
+    private PictureBox pictureBox3;
+    private PictureBox pictureBox4;
+    private PictureBox pictureBox5;
+    private PictureBox pictureBox6;
+    private PictureBox pictureBox7;
+    private PictureBox pictureBox8;
+    private PictureBox pictureBox9;
+    private PictureBox pictureBox10;
 }
 /*
 =======
