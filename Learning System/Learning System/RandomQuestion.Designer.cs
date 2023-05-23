77f49506326b33b23acdfba1d540a1069ce879c3
elio-perlman-13 <nat12102003@gmail.com>
2023-05-23 22:59:52 +0700

    thêm user control random question

--- Learning System/Learning System/RandomQuestion.Designer.cs
+++ Learning System/Learning System/RandomQuestion.Designer.cs
@@ -34,6 +34,13 @@
             RandomQuestion_SubcategoriesCbx = new CheckBox();
             RandomQuestion_NumberOfQuestionLbl = new Label();
             RandomQuestion_AddBtn = new Button();
+            RandomQuestion_ButtonPanelPnl = new Panel();
+            RandomQuestion_ShowQuestionDgv = new DataGridView();
+            Icon = new DataGridViewImageColumn();
+            Content = new DataGridViewTextBoxColumn();
+            textBox1 = new TextBox();
+            RandomQuestions_NumberOfRdQuestionsCbo = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)RandomQuestion_ShowQuestionDgv).BeginInit();
             SuspendLayout();
             // 
             // RandomQuestion_Header
@@ -78,12 +85,13 @@
             RandomQuestion_SubcategoriesCbx.TabIndex = 26;
             RandomQuestion_SubcategoriesCbx.Text = "Include questions from subcategories too";
             RandomQuestion_SubcategoriesCbx.UseVisualStyleBackColor = true;
+            RandomQuestion_SubcategoriesCbx.CheckedChanged += RandomQuestion_SubcategoriesCbx_CheckedChanged;
             // 
             // RandomQuestion_NumberOfQuestionLbl
             // 
             RandomQuestion_NumberOfQuestionLbl.AutoSize = true;
             RandomQuestion_NumberOfQuestionLbl.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            RandomQuestion_NumberOfQuestionLbl.Location = new Point(13, 327);
+            RandomQuestion_NumberOfQuestionLbl.Location = new Point(13, 174);
             RandomQuestion_NumberOfQuestionLbl.Name = "RandomQuestion_NumberOfQuestionLbl";
             RandomQuestion_NumberOfQuestionLbl.Size = new Size(236, 23);
             RandomQuestion_NumberOfQuestionLbl.TabIndex = 27;
@@ -96,7 +104,7 @@
             RandomQuestion_AddBtn.FlatStyle = FlatStyle.Flat;
             RandomQuestion_AddBtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             RandomQuestion_AddBtn.ForeColor = Color.White;
-            RandomQuestion_AddBtn.Location = new Point(13, 568);
+            RandomQuestion_AddBtn.Location = new Point(13, 678);
             RandomQuestion_AddBtn.Name = "RandomQuestion_AddBtn";
             RandomQuestion_AddBtn.Size = new Size(393, 40);
             RandomQuestion_AddBtn.TabIndex = 28;
@@ -104,10 +112,79 @@
             RandomQuestion_AddBtn.UseVisualStyleBackColor = false;
             RandomQuestion_AddBtn.Click += RandomQuestion_AddBtn_Click;
             // 
+            // RandomQuestion_ButtonPanelPnl
+            // 
+            RandomQuestion_ButtonPanelPnl.AutoSize = true;
+            RandomQuestion_ButtonPanelPnl.Location = new Point(13, 247);
+            RandomQuestion_ButtonPanelPnl.Name = "RandomQuestion_ButtonPanelPnl";
+            RandomQuestion_ButtonPanelPnl.Size = new Size(500, 50);
+            RandomQuestion_ButtonPanelPnl.TabIndex = 29;
+            // 
+            // RandomQuestion_ShowQuestionDgv
+            // 
+            RandomQuestion_ShowQuestionDgv.AllowUserToAddRows = false;
+            RandomQuestion_ShowQuestionDgv.AllowUserToDeleteRows = false;
+            RandomQuestion_ShowQuestionDgv.AllowUserToResizeColumns = false;
+            RandomQuestion_ShowQuestionDgv.AllowUserToResizeRows = false;
+            RandomQuestion_ShowQuestionDgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            RandomQuestion_ShowQuestionDgv.BackgroundColor = SystemColors.Control;
+            RandomQuestion_ShowQuestionDgv.BorderStyle = BorderStyle.Fixed3D;
+            RandomQuestion_ShowQuestionDgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            RandomQuestion_ShowQuestionDgv.ColumnHeadersVisible = false;
+            RandomQuestion_ShowQuestionDgv.Columns.AddRange(new DataGridViewColumn[] { Icon, Content });
+            RandomQuestion_ShowQuestionDgv.GridColor = SystemColors.Control;
+            RandomQuestion_ShowQuestionDgv.Location = new Point(13, 317);
+            RandomQuestion_ShowQuestionDgv.Name = "RandomQuestion_ShowQuestionDgv";
+            RandomQuestion_ShowQuestionDgv.ReadOnly = true;
+            RandomQuestion_ShowQuestionDgv.RowHeadersVisible = false;
+            RandomQuestion_ShowQuestionDgv.RowHeadersWidth = 51;
+            RandomQuestion_ShowQuestionDgv.RowTemplate.Height = 29;
+            RandomQuestion_ShowQuestionDgv.Size = new Size(1510, 282);
+            RandomQuestion_ShowQuestionDgv.TabIndex = 30;
+            // 
+            // Icon
+            // 
+            Icon.HeaderText = "Column1";
+            Icon.MinimumWidth = 6;
+            Icon.Name = "Icon";
+            Icon.ReadOnly = true;
+            Icon.Width = 150;
+            // 
+            // Content
+            // 
+            Content.HeaderText = "";
+            Content.MinimumWidth = 6;
+            Content.Name = "Content";
+            Content.ReadOnly = true;
+            Content.Width = 1350;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(13, 218);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(339, 23);
+            textBox1.TabIndex = 31;
+            textBox1.Text = "Questions matching this filter";
+            // 
+            // RandomQuestions_NumberOfRdQuestionsCbo
+            // 
+            RandomQuestions_NumberOfRdQuestionsCbo.FormattingEnabled = true;
+            RandomQuestions_NumberOfRdQuestionsCbo.Location = new Point(274, 173);
+            RandomQuestions_NumberOfRdQuestionsCbo.Name = "RandomQuestions_NumberOfRdQuestionsCbo";
+            RandomQuestions_NumberOfRdQuestionsCbo.Size = new Size(151, 28);
+            RandomQuestions_NumberOfRdQuestionsCbo.TabIndex = 32;
+            // 
             // RandomQuestion
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(RandomQuestions_NumberOfRdQuestionsCbo);
+            Controls.Add(textBox1);
+            Controls.Add(RandomQuestion_ShowQuestionDgv);
+            Controls.Add(RandomQuestion_ButtonPanelPnl);
             Controls.Add(RandomQuestion_AddBtn);
             Controls.Add(RandomQuestion_NumberOfQuestionLbl);
             Controls.Add(RandomQuestion_SubcategoriesCbx);
@@ -115,7 +192,8 @@
             Controls.Add(RandomQuestion_CategoryLbl);
             Controls.Add(RandomQuestion_Header);
             Name = "RandomQuestion";
-            Size = new Size(1428, 623);
+            Size = new Size(1647, 808);
+            ((System.ComponentModel.ISupportInitialize)RandomQuestion_ShowQuestionDgv).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -128,5 +206,11 @@
         private CheckBox RandomQuestion_SubcategoriesCbx;
         private Label RandomQuestion_NumberOfQuestionLbl;
         private Button RandomQuestion_AddBtn;
+        private Panel RandomQuestion_ButtonPanelPnl;
+        private DataGridView RandomQuestion_ShowQuestionDgv;
+        private DataGridViewImageColumn Icon;
+        private DataGridViewTextBoxColumn Content;
+        private TextBox textBox1;
+        private ComboBox RandomQuestions_NumberOfRdQuestionsCbo;
     }
 }
--- Learning System/Learning System/RandomQuestion.cs
+++ Learning System/Learning System/RandomQuestion.cs
@@ -1,4 +1,5 @@
 ﻿using Learning_System.ExternalClass;
+using Microsoft.Office.Interop.Word;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +20,26 @@ namespace Learning_System
         public DataProcessing categoriesData = new();
         public JArray _categoriesDataJarray = new();
 
+        //Categories data
+        private List<Categories> categories = new List<Categories>();
+        private List<int> selectedCategoriesIdList = new List<int>();
+        private bool selectQuestionsFromSubcategories = false;
+
+        //Page button data
+        private const int NUMBER_OF_PAGES_PER_GROUP = 5;
+        private const int NUMBER_OF_QUESTIONS_PER_PAGE = 10;
+        private Button[] PageButton = new Button[NUMBER_OF_PAGES_PER_GROUP + 4];
+        private int numberOfPage;
+        private int numberOfGroup;
+        private int pageSelectedIndex;
+        private int groupSelectedIndex;
+
+        //DataGridView data
+        private List<int> selectedQuestions = new List<int>();
+        private List<string> selectedQuestionsContent = new List<string>();
+
+        //RandomQuestionData
+        private List<int> randomSelectedQuestions = new List<int>();
         public void AddSpace(ref List<Categories> List, ref List<Categories> addList, int begin, string space)
         {
             foreach (int x in addList[begin].SubArray)
@@ -39,6 +61,7 @@ namespace Learning_System
             {
                 listCategories = _categoriesDataJarray.ToObject<List<Categories>>();
                 if (listCategories == null) return;
+                categories = listCategories;
                 AddSpace(ref newListCategories, ref listCategories, 0, "  ");
                 newListCategories.Reverse();
             }
@@ -47,16 +70,54 @@ namespace Learning_System
                 DialogResult dialog = MessageBox.Show("Can't get categories data:\n" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
                 if (dialog == DialogResult.OK)
-                    Application.Exit();
+                    System.Windows.Forms.Application.Exit();
             }
 
             RandomQuestion_CategoryCbo.ValueMember = "Id";
             RandomQuestion_CategoryCbo.DisplayMember = "Name";
             RandomQuestion_CategoryCbo.DataSource = newListCategories;
         }
+        private void RandomQuestion_CategoryCbo_Click(object sender, EventArgs e)
+        {
+            loadCategoriesData();
+        }
+        private void GetSubCategories(int _parentCategories, ref List<int> _subCategories, ref List<Categories> categories)
+        {
+            foreach (int x in categories[_parentCategories].SubArray)
+            {
+                _subCategories.Add(x);
+                GetSubCategories(x, ref _subCategories, ref categories);
+            }
+        }
         public RandomQuestion()
         {
             InitializeComponent();
+
+            RandomQuestion_CategoryCbo.SelectedIndex = -1;
+            RandomQuestion_CategoryCbo.SelectedText = "  Default";
+
+            for (int i = 0; i < NUMBER_OF_PAGES_PER_GROUP + 4; i++)
+            {
+                PageButton[i] = new Button();
+                PageButton[i].Location = new System.Drawing.Point(RandomQuestion_ButtonPanelPnl.Left + i * 50, 0);
+                PageButton[i].Size = new Size(50, 50);
+                if (i == 0 || i == NUMBER_OF_PAGES_PER_GROUP + 3)
+                {
+                    if (i == 0) PageButton[i].Text = "<";
+                    else PageButton[i].Text = ">";
+                    PageButton[i].Font = new System.Drawing.Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+                }
+                else
+                {
+                    if ((i == 1) || (i == NUMBER_OF_PAGES_PER_GROUP + 2)) PageButton[i].Text = "...";
+                    PageButton[i].Font = new System.Drawing.Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+                }
+                PageButton[i].ForeColor = Color.LightSkyBlue;
+                PageButton[i].Visible = true;
+                PageButton[i].AutoSize = true;
+                PageButton[i].Click += new EventHandler(PageButtonHandler);
+                RandomQuestion_ButtonPanelPnl.Controls.Add(PageButton[i]);
+            }
         }
         public void addData(DataProcessing _questionData, DataProcessing _categoriesData, JArray _categoriesDataJarray)
         {
@@ -67,17 +128,219 @@ namespace Learning_System
 
         private void RandomQuestion_AddBtn_Click(object sender, EventArgs e)
         {
+            if (RandomQuestions_NumberOfRdQuestionsCbo.SelectedItem == null)
+            {
+                MessageBox.Show("Chưa chọn số câu hỏi cần được thêm", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (selectedQuestions.Count == 0 || selectedQuestionsContent.Count == 0)
+            {
+                MessageBox.Show("Không tồn tại các câu hỏi cần được thêm trong Category đã chọn", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string text = "Đã thêm thành công những câu hỏi có ID sau vào contest: ";
+            for (int i = 0; i <= RandomQuestions_NumberOfRdQuestionsCbo.SelectedIndex; i++)
+            {
+                Random rd = new Random();
+                int index = rd.Next(0, selectedQuestions.Count - 1);
+                randomSelectedQuestions.Add(selectedQuestions[index]);
+                text += selectedQuestions[index].ToString();
+                if (i == RandomQuestions_NumberOfRdQuestionsCbo.SelectedIndex) text += ".";
+                else text += ", ";
+                selectedQuestions.RemoveAt(index);
+                selectedQuestionsContent.RemoveAt(index);
+            }
+            MessageBox.Show(text, "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
             this.SendToBack();
         }
 
         private void RandomQuestion_CategoryCbo_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (RandomQuestion_CategoryCbo.SelectedItem == null) return;
+            var a = (Categories)RandomQuestion_CategoryCbo.SelectedItem;
+            selectedCategoriesIdList.Clear();
+            int _parentCategories = a.Id;
+            try
+            {
+                if (categories == null || _categoriesDataJarray == null) return;
+                if (_parentCategories >= 0)
+                {
+                    selectedCategoriesIdList.Add(_parentCategories);
+                    GetSubCategories(_parentCategories, ref selectedCategoriesIdList, ref categories);
+                    SelectQuestionsFromCategories(selectedCategoriesIdList, selectQuestionsFromSubcategories);
+                    //                    showQuestions(selectedCategoriesIdList, showQuestionsFromSubcategories);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogResult dialog = MessageBox.Show("Can't get categories data:\n" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
+                if (dialog == DialogResult.OK)
+                    System.Windows.Forms.Application.Exit();
+            }
         }
 
-        private void RandomQuestion_CategoryCbo_Click(object sender, EventArgs e)
+        private void RandomQuestion_SubcategoriesCbx_CheckedChanged(object sender, EventArgs e)
         {
-            loadCategoriesData();
+            selectQuestionsFromSubcategories = RandomQuestion_SubcategoriesCbx.Checked;
+            SelectQuestionsFromCategories(selectedCategoriesIdList, selectQuestionsFromSubcategories);
+            //            showQuestions(selectedCategoriesIdList, showQuestionsFromSubcategories);
+        }
+
+        private void SelectQuestionsFromCategories(List<int> _selectedCategoriesIdList, bool _selectQuestionsFromSubcategories)
+        {
+            if (selectedQuestions == null) return;
+            selectedQuestions.Clear();
+            selectedQuestionsContent.Clear();
+
+            try
+            {
+                for (int i = 0; i < questionsData.Length(); i++)
+                {
+                    DataRow? question = questionsData.Init().Offset(i).Limit(1).GetFirstRow();
+                    if (question != null)
+                    {
+                        int containedInCategories = question.Field<int>("CategoryID");
+                        if (_selectQuestionsFromSubcategories && _selectedCategoriesIdList.Contains(containedInCategories) || (!_selectQuestionsFromSubcategories && _selectedCategoriesIdList[0] == containedInCategories))
+                        {
+                            selectedQuestions.Add(question.Field<int>("ID"));
+                            string? s = question.Field<string>("Content");
+                            if (s == null) s = "";
+                            string _content;
+                            RichTextBox tmp = new RichTextBox();
+                            tmp.Visible = false;
+                            try
+                            {
+                                tmp.Rtf = s;
+                                _content = tmp.Text;
+                            }
+                            catch
+                            {
+                                _content = s;
+                            }
+                            if (question.Field<string>("Name") != "") _content = question.Field<string>("Name") + ": " + _content;
+                            selectedQuestionsContent.Add(_content);
+                        }
+                    }
+                }
+                if (selectedQuestions.Count % NUMBER_OF_QUESTIONS_PER_PAGE == 0) numberOfPage = selectedQuestions.Count / NUMBER_OF_QUESTIONS_PER_PAGE;
+                else numberOfPage = selectedQuestions.Count / NUMBER_OF_QUESTIONS_PER_PAGE + 1;
+                if (numberOfPage <= 0) return;
+                if (numberOfPage % (NUMBER_OF_PAGES_PER_GROUP) == 0) numberOfGroup = numberOfPage / (NUMBER_OF_PAGES_PER_GROUP);
+                else numberOfGroup = (numberOfPage / (NUMBER_OF_PAGES_PER_GROUP)) + 1;
+                groupSelectedIndex = 1;
+                pageSelectedIndex = 1;
+                ShowButtons(pageSelectedIndex, groupSelectedIndex);
+                ShowQuestion(pageSelectedIndex, selectedQuestions);
+
+                RandomQuestions_NumberOfRdQuestionsCbo.Items.Clear();
+                for (int i = 1; i <= selectedQuestions.Count; i++)
+                {
+                    RandomQuestions_NumberOfRdQuestionsCbo.Items.Add(i);
+                }
+            }
+            catch (Exception ex)
+            {
+                DialogResult dialog = MessageBox.Show("Can't get questions data:\n" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                if (dialog == DialogResult.OK)
+                    System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        private void ShowButtons(int pageSelectedIndex, int groupSelectedIndex)
+        {
+            for (int i = 2; i < NUMBER_OF_PAGES_PER_GROUP + 2; i++)
+            {
+                int currentPage = (groupSelectedIndex - 1) * (NUMBER_OF_PAGES_PER_GROUP) + (i - 1);
+                if (pageSelectedIndex == currentPage)
+                {
+                    PageButton[i].BackColor = Color.LightSkyBlue;
+                    PageButton[i].ForeColor = Color.White;
+                }
+                else
+                {
+                    PageButton[i].ForeColor = Color.LightSkyBlue;
+                    PageButton[i].BackColor = Color.White;
+                }
+                if (currentPage > numberOfPage) PageButton[i].Text = " ";
+                else PageButton[i].Text = (currentPage).ToString();
+            }
+            if ((groupSelectedIndex) == 1) PageButton[1].Text = " ";
+            else PageButton[1].Text = "...";
+            if (groupSelectedIndex == numberOfGroup) PageButton[NUMBER_OF_PAGES_PER_GROUP + 2].Text = " ";
+            else PageButton[NUMBER_OF_PAGES_PER_GROUP + 2].Text = "...";
+        }
+        private void ShowQuestion(int pageSelectedIndex, List<int> selectedQuestions)
+        {
+            RandomQuestion_ShowQuestionDgv.Rows.Clear();
+            for (int i = (pageSelectedIndex - 1) * (NUMBER_OF_QUESTIONS_PER_PAGE); i < pageSelectedIndex * NUMBER_OF_QUESTIONS_PER_PAGE; i++)
+            {
+                if (i >= selectedQuestions.Count) break;
+                var index = RandomQuestion_ShowQuestionDgv.Rows.Add();
+                DataGridViewRow row = RandomQuestion_ShowQuestionDgv.Rows[index];
+                row.Cells[1].Value = selectedQuestionsContent[i];
+            }
+            RandomQuestion_ShowQuestionDgv.AutoResizeRows(DataGridViewAutoSizeRowsMode.DisplayedCells);
+        }
+        private void PageButtonHandler(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            if (b == null || b.Text == " " || pageSelectedIndex < 1 || pageSelectedIndex > numberOfPage) return;
+            if (b.Text == "<")
+            {
+                if (pageSelectedIndex <= 1) return;
+                pageSelectedIndex--;
+                if (pageSelectedIndex % NUMBER_OF_PAGES_PER_GROUP == 0)
+                {
+                    groupSelectedIndex--;
+                }
+                ShowButtons(pageSelectedIndex, groupSelectedIndex);
+                ShowQuestion(pageSelectedIndex, selectedQuestions);
+                return;
+            }
+            if (b.Text == ">")
+            {
+                if (pageSelectedIndex >= numberOfPage) return;
+                pageSelectedIndex++;
+                if (pageSelectedIndex % NUMBER_OF_PAGES_PER_GROUP == 1 || NUMBER_OF_PAGES_PER_GROUP == 1)
+                {
+                    groupSelectedIndex++;
+                }
+                ShowButtons(pageSelectedIndex, groupSelectedIndex);
+                ShowQuestion(pageSelectedIndex, selectedQuestions);
+                return;
+            }
+            if (b.Text == "...")
+            {
+                if (PageButton[1] == b)
+                {
+                    if (groupSelectedIndex <= 1) return;
+                    groupSelectedIndex--;
+                    pageSelectedIndex--;
+                    ShowButtons(pageSelectedIndex, groupSelectedIndex);
+                    PageButton[NUMBER_OF_PAGES_PER_GROUP + 1].PerformClick();
+                    return;
+                }
+                if (PageButton[NUMBER_OF_PAGES_PER_GROUP + 2] == b)
+                {
+                    if (groupSelectedIndex >= numberOfGroup) return;
+                    groupSelectedIndex++;
+                    pageSelectedIndex++;
+                    ShowButtons(pageSelectedIndex, groupSelectedIndex);
+                    PageButton[2].PerformClick();
+                    return;
+                }
+            }
+            int _pageSelectedIndex;
+            bool isConvertable = int.TryParse(b.Text, out _pageSelectedIndex);
+            if (isConvertable)
+            {
+                pageSelectedIndex = _pageSelectedIndex;
+                ShowButtons(pageSelectedIndex, groupSelectedIndex);
+                ShowQuestion(pageSelectedIndex, selectedQuestions);
+            }
+            return;
         }
     }
 }
